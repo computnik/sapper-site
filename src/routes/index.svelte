fc11d7906e443d521f4de32ce51be1c97efdf497
Nikhil Motiani <motianinikhil@gmail.com>
2020-07-12 19:04:57 +0530

    feat(styles): added tailwind integration

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,15 +38,10 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Success Kid" src="successkid.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<div class="flex items-center justify-center h-screen">
+  <div
+    class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10"
+  >
+    Nikhil Motiani
+  </div>
+</div>
